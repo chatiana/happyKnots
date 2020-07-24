dde13f9de1c41ea5971e3bc546f199f6d5b71515
chatiana <thatiana.cm@gmail.com>
2020-07-24 01:45:10 +0100

    adding authentication, login, registration, about, contact, sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,43 @@
+const user = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
+ // const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1];
+  console.log(req.session.isLoggedIn);
     res.render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn
+      isAuthenticated: false
     });
   };
-  /*
+ 
   exports.postLogin = (req, res, next) => {
-      req.isLoggedIn = true;
+    user.findById('5f1848443fc02a16c9d6f671')
+    .then(user => {
+      req.session.isLoggedIn = true; //Expires=, Max-age=, Domain=, Secure, HttpOnly
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+  };
+  
+  exports.postLogout = (req, res, next) => {
+    req.session.destroy (() => {
+     // console.log(err);
       res.redirect('/');
-    };
-  */
+    });
+  };
+
+  exports.getRegister = (req, res, next) => {
+     console.log(req.session.isLoggedIn);
+       res.render('auth/register', {
+         path: '/register',
+         pageTitle: 'Register',
+         isAuthenticated: false
+       });
+     };
+
+     exports.postRegister = (req, res, next) => {
+     }
--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,7 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('error404', { pageTitle: 'Page Not Found', path:'/error404'});
+    res.status(404).render('error404', { 
+      pageTitle: 'Page Not Found', 
+      path:'/error404',
+      isAuthenticated: req.isLoggedIn 
+    });
   };
