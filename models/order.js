90eb8818255382c8805672b63b2f7437e58e2e48
chatiana <thatiana.cm@gmail.com>
2020-07-26 23:48:02 +0100

    recent changes in authorization, password @ git push origin master

--- models/order.js
+++ models/order.js
@@ -3,15 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    products:[{  //array of documents
-        product: { type: Object, required: true },
-        quantity: { type: Number, required: true },
-        price:{ type: Number, /*required: true */}
-    }],
-    user:{
-        name: { type: String, required: true },
-        userId: { type: Schema.Types.ObjectId, required: true, ref:'User'
-     } //will refer to user model
+  products: [ //array of documents
+    {
+      product: { type: Object, required: true },
+      quantity: { type: Number, required: true }
     }
+  ],
+  user: {
+    email: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    } //will refer to user model
+  }
 });
+
 module.exports = mongoose.model('Order', orderSchema);
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 
 const  shopController = require('../controllers/shop');
+//middleware to protect routes
+const isAuth = require('../middleware/is-auth');  
 
 const router = express.Router();
 
@@ -13,13 +15,11 @@ router.get('/contact', shopController.getContact);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct); //Product Details
 
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-router.post('/create-order', shopController.postOrder);
-router.get('/orders', shopController.getOrders);
-// router.get('/checkout', shopController.getCheckout);
-
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+router.post('/create-order',isAuth, shopController.postOrder);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
