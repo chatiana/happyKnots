33c3ae4d551fa149aeadf192e4a69efdbfff5be3
chatiana <thatiana.cm@gmail.com>
2020-07-29 04:19:08 +0100

    removing sensitive data

--- routes/auth.js
+++ routes/auth.js
@@ -37,7 +37,7 @@ router.post(
     [
         check('email')
             .isEmail()
-            .withMessage('Please enter a valid email.')
+            .withMessage('Please enter a valid email address.')
             .custom((value, { req }) => {
                 // if (value === 'test@test.com') {
                 //   throw new Error('This email address if forbidden.');
